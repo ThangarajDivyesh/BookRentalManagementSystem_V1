d47e5512a63a827222c5a3b1603171e5673746f5
ThangarajDivyesh <dithakshan7529@gmail.com>
2024-10-11 14:19:23 +0530

    Q3

--- BookRentalManagementSystem_V1/BookManager.cs
+++ BookRentalManagementSystem_V1/BookManager.cs
@@ -8,5 +8,26 @@ namespace BookRentalManagementSystem_V1
 {
     internal class BookManager
     {
+        public List<Book> books=new List<Book>();
+
+        public void CreateBook(Book book)
+        {
+
+          books.Add(book);
+            Console.WriteLine("Book added successfully");
+        }
+
+        public void ReadBook()
+        {
+            if (books.Count == 0)
+            {
+                Console.WriteLine("List of books");
+            }
+            foreach (Book book in books)
+            {
+                Console.WriteLine(book);
+            }
+        }
+
     }
 }
